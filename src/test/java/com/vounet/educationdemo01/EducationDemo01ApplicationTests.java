a60c98056239643e82cdb9afb6899ee685d41543
HIM198 <87613491+198HIM@users.noreply.github.com>
2023-03-03 11:56:13 +0800

    第一次提交

---
+++ src/test/java/com/vounet/educationdemo01/EducationDemo01ApplicationTests.java
@@ -0,0 +1,23 @@
+package com.vounet.educationdemo01;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+
+@SpringBootTest
+@Slf4j
+class EducationDemo01ApplicationTests {
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    void contextLoads() throws SQLException {
+        log.info("数据库链接"+dataSource.getConnection().toString());
+    }
+
+}
